SonicMastr/libraries

Update SceNetPs (#376)
Update SceNetPs, net_syscalls.h, net.h

include/kernel/net/net.h
@@ -543,12 +543,12 @@ int sceNetConnect(int s, const SceNetSockaddr *name, unsigned int namelen);
 int sceNetListen(int s, int backlog);
 int sceNetRecvfrom(int s, void *buf, unsigned int len, int flags, SceNetSockaddr *from, unsigned int *fromlen);
 int sceNetSendto(int s, const void *msg, unsigned int len, int flags, const SceNetSockaddr *to, unsigned int tolen);
+int sceNetSetsockopt(int s, int level, int optname, const void *optval, unsigned int optlen);
+int sceNetClose(int s);
 
 #define sceNetRecv(s, buf, len, flags) sceNetRecvfrom(s, buf, len, flags, NULL, 0)
 #define sceNetSend(s, msg, len, flags) sceNetSendto(s, msg, len, flags, NULL, 0)
-
-int sceNetSetsockopt(int s, int level, int optname, const void *optval, unsigned int optlen);
-int sceNetSocketClose(int s);
+#define sceNetSocketClose sceNetClose
 
 /* fixme ? */
 #define sceNetHtons __builtin_bswap16

Rinnegatamante/vita-headers

include/psp2kern/net/net.h
@@ -549,12 +549,12 @@ int ksceNetConnect(int s, const SceNetSockaddr *name, unsigned int namelen);
 int ksceNetListen(int s, int backlog);
 int ksceNetRecvfrom(int s, void *buf, unsigned int len, int flags, SceNetSockaddr *from, unsigned int *fromlen);
 int ksceNetSendto(int s, const void *msg, unsigned int len, int flags, const SceNetSockaddr *to, unsigned int tolen);
+int ksceNetSetsockopt(int s, int level, int optname, const void *optval, unsigned int optlen);
+int ksceNetClose(int s);
 
 #define ksceNetRecv(s, buf, len, flags) ksceNetRecvfrom(s, buf, len, flags, NULL, 0)
 #define ksceNetSend(s, msg, len, flags) ksceNetSendto(s, msg, len, flags, NULL, 0)
-
-int ksceNetSetsockopt(int s, int level, int optname, const void *optval, unsigned int optlen);
-int ksceNetSocketClose(int s);
+#define ksceNetSocketClose ksceNetClose
 
 /* fixme ? */
 #define ksceNetHtons __builtin_bswap16