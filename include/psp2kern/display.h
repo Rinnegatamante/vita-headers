SonicMastr/libraries

Changed sceDisplayGetProcFrameBufInternal header

include/kernel/display.h
@@ -109,7 +109,7 @@ int sceDisplayGetFrameBuf(SceDisplayFrameBuf *pParam, int sync);
  *
  * @return 0 on success, < 0 on error.
 */
-int sceDisplayGetFrameBufInfoForPid(SceUID pid, int head, int index, SceDisplayFrameBufInfo *info);
+int sceDisplayGetProcFrameBufInternal(SceUID pid, int head, int index, SceDisplayFrameBufInfo *info);
 
 /**
  * Get maximum framebuffer resolution

Rinnegatamante/vita-headers

include/psp2kern/display.h
@@ -115,7 +115,7 @@ int ksceDisplayGetFrameBuf(SceDisplayFrameBuf *pParam, int sync);
  *
  * @return 0 on success, < 0 on error.
 */
-int ksceDisplayGetFrameBufInfoForPid(SceUID pid, int head, int index, SceDisplayFrameBufInfo *info);
+int ksceDisplayGetProcFrameBufInternal(SceUID pid, int head, int index, SceDisplayFrameBufInfo *info);
 
 /**
  * Get maximum framebuffer resolution