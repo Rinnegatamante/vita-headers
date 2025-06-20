SonicMastr/libraries

Fix ksceKernelCpuUnrestrictedMemcpy alignment mistake. (#468)
* Fix ksceKernelCpuUnrestrictedMemcpy alignment mistake.

* Remove unnecessary if branches

include/kernel/kernel/cpu.h
@@ -94,7 +94,13 @@ static inline int sceKernelCpuUnrestrictedMemcpy(void *dst, const void *src, Sce
 	asm volatile("mcr p15, 0, %0, c3, c0, 0" :: "r" (0xFFFF0000));
 
 	memcpy(dst, src, len);
-	sceKernelCpuDcacheWritebackRange((void *)((uintptr_t)dst & ~0x1F), (len + 0x1F) & ~0x1F);
+
+	len += (SceSize)(((uintptr_t)dst) & 0x1F);
+
+	dst = (void *)(((uintptr_t)dst) & ~0x1F);
+	len = (len + 0x1F) & ~0x1F;
+
+	sceKernelCpuDcacheWritebackRange(dst, len);
 
 	asm volatile("mcr p15, 0, %0, c3, c0, 0" :: "r" (prev_dacr));
 	return 0;

Rinnegatamante/vita-headers

include/psp2kern/kernel/cpu.h
@@ -100,7 +100,13 @@ static inline int ksceKernelCpuUnrestrictedMemcpy(void *dst, const void *src, Sc
 	asm volatile("mcr p15, 0, %0, c3, c0, 0" :: "r" (0xFFFF0000));
 
 	memcpy(dst, src, len);
-	ksceKernelCpuDcacheWritebackRange((void *)((uintptr_t)dst & ~0x1F), (len + 0x1F) & ~0x1F);
+
+	len += (SceSize)(((uintptr_t)dst) & 0x1F);
+
+	dst = (void *)(((uintptr_t)dst) & ~0x1F);
+	len = (len + 0x1F) & ~0x1F;
+
+	ksceKernelCpuDcacheWritebackRange(dst, len);
 
 	asm volatile("mcr p15, 0, %0, c3, c0, 0" :: "r" (prev_dacr));
 	return 0;