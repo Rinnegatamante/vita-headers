SonicMastr/libraries

Add some functions of SceKernelUtilsForDriver (#350)
* Add some functions of SceKernelUtilsForDriver

* Add some nid of SceKernelUtilsForDriver

include/kernel/kernel/utils.h
@@ -60,6 +60,10 @@ int sceSha256BlockUpdate(SceSha256Context *pContext, const void *plain, uint32_t
 int sceSha256BlockResult(SceSha256Context *pContext, char *digest);
 int sceSha256Digest(const void *plain, uint32_t len, char *result);
 
+int sceHmacSha1Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
+int sceHmacSha224Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
+int sceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
+
 #ifdef __cplusplus
 }
 #endif

Rinnegatamante/vita-headers

include/psp2kern/kernel/utils.h
@@ -66,6 +66,10 @@ int ksceSha256BlockUpdate(SceSha256Context *pContext, const void *plain, uint32_
 int ksceSha256BlockResult(SceSha256Context *pContext, char *digest);
 int ksceSha256Digest(const void *plain, uint32_t len, char *result);
 
+int ksceHmacSha1Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
+int ksceHmacSha224Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
+int ksceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
+
 #ifdef __cplusplus
 }
 #endif