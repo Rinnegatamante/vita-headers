SonicMastr/libraries

Add ksceKernelGetMutexInfo_089 (#389)
* Add ksceKernelGetMutexInfo_089

https://wiki.henkaku.xyz/vita/SceKernelThreadMgr#sceKernelGetMutexInfo_089ForDriver

* Remove _089 suffix for kscekernelGetMutexInfo

include/kernel/kernel/threadmgr.h
@@ -439,6 +439,16 @@ int sceKernelUnlockMutex(SceUID mutexid, int unlockCount);
  */
 int sceKernelCancelMutex(SceUID mutexid, int newCount, int *numWaitThreads);
 
+/**
+ * Retrieve information about a mutex.
+ *
+ * @param mutexid - UID of the mutex to retrieve info for.
+ * @param info - Pointer to a ::SceKernelMutexInfo struct to receive the info.
+ *
+ * @return < 0 on error.
+ */
+int sceKernelGetMutexInfo(SceUID mutexid, SceKernelMutexInfo *info);
+
 typedef struct  SceKernelLwMutexWork {
 	SceInt64 data[4];
 } SceKernelLwMutexWork;

Rinnegatamante/vita-headers

include/psp2kern/kernel/threadmgr.h
@@ -445,6 +445,16 @@ int ksceKernelUnlockMutex(SceUID mutexid, int unlockCount);
  */
 int ksceKernelCancelMutex(SceUID mutexid, int newCount, int *numWaitThreads);
 
+/**
+ * Retrieve information about a mutex.
+ *
+ * @param mutexid - UID of the mutex to retrieve info for.
+ * @param info - Pointer to a ::SceKernelMutexInfo struct to receive the info.
+ *
+ * @return < 0 on error.
+ */
+int ksceKernelGetMutexInfo(SceUID mutexid, SceKernelMutexInfo *info);
+
 typedef struct  SceKernelLwMutexWork {
 	SceInt64 data[4];
 } SceKernelLwMutexWork;