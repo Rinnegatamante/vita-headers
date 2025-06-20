SonicMastr/libraries

Add ksceGzipDecompress (#353)
Add ksceGzipDecompress

include/kernel/kernel/utils.h
@@ -64,6 +64,16 @@ int sceHmacSha1Digest(const unsigned char *key, uint32_t key_len, const void *pl
 int sceHmacSha224Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
 int sceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
 
+/**
+ * @param[out] dst - dst buf
+ * @param[in] dst_size - Size when decompressed
+ * @param[in] src - Gzip compressed data
+ * @param[out] crc32 - crc32 when decompressed
+ *
+ * @return dst_size on success, < 0 on error.
+ */
+int sceGzipDecompress(void *dst, int dst_size, const void *src, int *crc32);
+
 #ifdef __cplusplus
 }
 #endif

Rinnegatamante/vita-headers

include/psp2kern/kernel/utils.h
@@ -70,6 +70,16 @@ int ksceHmacSha1Digest(const unsigned char *key, uint32_t key_len, const void *p
 int ksceHmacSha224Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
 int ksceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
 
+/**
+ * @param[out] dst - dst buf
+ * @param[in] dst_size - Size when decompressed
+ * @param[in] src - Gzip compressed data
+ * @param[out] crc32 - crc32 when decompressed
+ *
+ * @return dst_size on success, < 0 on error.
+ */
+int ksceGzipDecompress(void *dst, int dst_size, const void *src, int *crc32);
+
 #ifdef __cplusplus
 }
 #endif