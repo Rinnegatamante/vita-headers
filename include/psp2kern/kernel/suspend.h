SonicMastr/libraries

Added ksceKernelRegisterSysEventHandler to header

include/kernel/kernel/suspend.h
@@ -7,6 +7,8 @@
 extern "C" {
 #endif
 
+typedef int (* SceSysEventHandler)(int resume, int eventid, void *args, void *opt);
+
 typedef enum SceKernelPowerTickType {
 	/** Cancel all timers */
 	SCE_KERNEL_POWER_TICK_DEFAULT			= 0,
@@ -27,6 +29,17 @@ typedef enum SceKernelPowerTickType {
  * @return 0
 */
 int sceKernelPowerTick(int type);
+
+/**
+ * Register system event handler
+ *
+ * @param[in] name - Name of handler
+ * @param[in] handler - The handler
+ * @param[in] args - Handler arguments
+ *
+ * @return 0 on success, < 0 on error.
+*/
+int sceKernelRegisterSysEventHandler(const char *name, SceSysEventHandler handler, void *args);
 
 #ifdef __cplusplus
 }

Rinnegatamante/vita-headers

include/psp2kern/kernel/suspend.h
@@ -13,6 +13,8 @@
 extern "C" {
 #endif
 
+typedef int (* SceSysEventHandler)(int resume, int eventid, void *args, void *opt);
+
 typedef enum SceKernelPowerTickType {
 	/** Cancel all timers */
 	SCE_KERNEL_POWER_TICK_DEFAULT			= 0,
@@ -33,6 +35,17 @@ typedef enum SceKernelPowerTickType {
  * @return 0
 */
 int ksceKernelPowerTick(int type);
+
+/**
+ * Register system event handler
+ *
+ * @param[in] name - Name of handler
+ * @param[in] handler - The handler
+ * @param[in] args - Handler arguments
+ *
+ * @return 0 on success, < 0 on error.
+*/
+int ksceKernelRegisterSysEventHandler(const char *name, SceSysEventHandler handler, void *args);
 
 #ifdef __cplusplus
 }