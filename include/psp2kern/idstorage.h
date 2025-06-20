SonicMastr/libraries

Fix many args (#451)
Changes document and variable types

include/kernel/idstorage.h
@@ -13,7 +13,7 @@ extern "C" {
  *
  * @return 0 on success, < 0 on error.
  */
-int sceIdStorageReadLeaf(int leafnum, void *buf);
+int sceIdStorageReadLeaf(SceSize leafnum, void *buf);
   
 /**
  * @param[in] leafnum - 0x0 ~ 0x80 / leafnum > 0x80 = error
@@ -21,7 +21,7 @@ int sceIdStorageReadLeaf(int leafnum, void *buf);
  *
  * @return 0 on success, < 0 on error.
  */
-int sceIdStorageWriteLeaf(int leafnum, const void *buf);
+int sceIdStorageWriteLeaf(SceSize leafnum, const void *buf);
 
 #ifdef __cplusplus
 }

include/kernel/kernel/processmgr.h
@@ -33,7 +33,7 @@ int sceKernelGetProcessLocalStorageAddrForPid(SceUID pid, int key, void **out_ad
  * @param[in]   unk Unknown.
  * @return      PID of the launched app on success, < 0 on error.
  */
-SceUID sceKernelLaunchApp(char* titleid, uint32_t flags, char *path, uint32_t unk);
+SceUID sceKernelLaunchApp(const char *titleid, uint32_t flags, const char *path, uint32_t unk);
 
 /**
  * @brief       Resume a suspended process.

include/kernel/kernel/utils.h
@@ -58,24 +58,24 @@ typedef struct {
 	uint8_t data[976];
 } SceAesContext;
 
-int sceSha1BlockInit(SceSha1Context *pContext);
-int sceSha1BlockUpdate(SceSha1Context *pContext, const void *plain, uint32_t len);
-int sceSha1BlockResult(SceSha1Context *pContext, char *digest);
-int sceSha1Digest(const void *plain, uint32_t len, char *result);
+int sceSha1BlockInit(SceSha1Context *ctx);
+int sceSha1BlockUpdate(SceSha1Context *ctx, const void *plain, SceSize len);
+int sceSha1BlockResult(SceSha1Context *ctx, void *result);
+int sceSha1Digest(const void *plain, SceSize len, void *digest);
 
-int sceSha224BlockInit(SceSha224Context *pContext);
-int sceSha224BlockUpdate(SceSha224Context *pContext, const void *plain, uint32_t len);
-int sceSha224BlockResult(SceSha224Context *pContext, char *digest);
-int sceSha224Digest(const void *plain, uint32_t len, char *result);
+int sceSha224BlockInit(SceSha224Context *ctx);
+int sceSha224BlockUpdate(SceSha224Context *ctx, const void *plain, SceSize len);
+int sceSha224BlockResult(SceSha224Context *ctx, void *result);
+int sceSha224Digest(const void *plain, SceSize len, void *digest);
 
-int sceSha256BlockInit(SceSha256Context *pContext);
-int sceSha256BlockUpdate(SceSha256Context *pContext, const void *plain, uint32_t len);
-int sceSha256BlockResult(SceSha256Context *pContext, char *digest);
-int sceSha256Digest(const void *plain, uint32_t len, char *result);
+int sceSha256BlockInit(SceSha256Context *ctx);
+int sceSha256BlockUpdate(SceSha256Context *ctx, const void *plain, SceSize len);
+int sceSha256BlockResult(SceSha256Context *ctx, void *result);
+int sceSha256Digest(const void *plain, SceSize len, void *digest);
 
-int sceHmacSha1Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
-int sceHmacSha224Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
-int sceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
+int sceHmacSha1Digest(const void *key, SceSize key_len, const void *plain, SceSize len, void *digest);
+int sceHmacSha224Digest(const void *key, SceSize key_len, const void *plain, SceSize len, void *digest);
+int sceHmacSha256Digest(const void *key, SceSize key_len, const void *plain, SceSize len, void *digest);
 
 /**
  * @param[out] dst - dst buf
@@ -85,7 +85,7 @@ int sceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void *
  *
  * @return decompressed size on success, < 0 on error.
  */
-int sceGzipDecompress(void *dst, uint32_t dst_size, const void *src, uint32_t *crc32);
+int sceGzipDecompress(void *dst, SceSize dst_size, const void *src, uint32_t *crc32);
 
 /**
  * @brief Check magic of Gzip header
@@ -167,7 +167,7 @@ const void *sceZlibGetCompressedData(const void *src);
  *
  * @return decompressed size on success, < 0 on error.
  */
-int sceZlibDecompress(void *dst, uint32_t dst_size, const void *src, uint32_t *adler32);
+int sceZlibDecompress(void *dst, SceSize dst_size, const void *src, uint32_t *adler32);
 
 /**
  * @param[out] dst - dst buf
@@ -177,8 +177,8 @@ int sceZlibDecompress(void *dst, uint32_t dst_size, const void *src, uint32_t *a
  *
  * @return decompressed size on success, < 0 on error.
  */
-int sceDeflateDecompress(void *dst, uint32_t dst_size, const void *src, const void **next);
-int sceDeflateDecompressPartial(void *dst, unsigned int dst_size, const void *src, const void **next, SceDeflatePartialInputParam *cbInfo);
+int sceDeflateDecompress(void *dst, SceSize dst_size, const void *src, const void **next);
+int sceDeflateDecompressPartial(void *dst, SceSize dst_size, const void *src, const void **next, SceDeflatePartialInputParam *cbInfo);
 
 /**
  * @param[out] ctx - out key data, etc...
@@ -188,9 +188,9 @@ int sceDeflateDecompressPartial(void *dst, unsigned int dst_size, const void *sr
  *
  * @return 0 on success, < 0 on error.
  */
-int sceAesInit1(SceAesContext *ctx, int blocksize, int keysize, const void *key);
-int sceAesInit2(SceAesContext *ctx, int blocksize, int keysize, const void *key);
-int sceAesInit3(SceAesContext *ctx, int blocksize, int keysize, const void *key);
+int sceAesInit1(SceAesContext *ctx, SceSize blocksize, SceSize keysize, const void *key);
+int sceAesInit2(SceAesContext *ctx, SceSize blocksize, SceSize keysize, const void *key);
+int sceAesInit3(SceAesContext *ctx, SceSize blocksize, SceSize keysize, const void *key);
 
 int sceAesDecrypt1(SceAesContext *ctx, const void *src, void *dst);
 int sceAesDecrypt2(SceAesContext *ctx, const void *src, void *dst);

include/kernel/registrymgr.h
@@ -17,7 +17,7 @@ extern "C" {
  *
  * @return 0 on success, < 0 on error
  */
-int sceRegMgrGetKeyBin(const char *category, const char *name, void *buf, int size);
+int sceRegMgrGetKeyBin(const char *category, const char *name, void *buf, SceSize size);
 
 /**
  * Get a key's information by category and name
@@ -28,7 +28,7 @@ int sceRegMgrGetKeyBin(const char *category, const char *name, void *buf, int si
  *
  * @return 0 on success, < 0 on error
  */
-int sceRegMgrGetKeyInt(const char* category, const char* name, int* buf);
+int sceRegMgrGetKeyInt(const char *category, const char *name, int *buf);
 
 /**
  * Get a key's information by category and name
@@ -40,7 +40,7 @@ int sceRegMgrGetKeyInt(const char* category, const char* name, int* buf);
  *
  * @return 0 on success, < 0 on error
  */
-int sceRegMgrGetKeyStr(const char* category, const char* name, char* buf, const int size);
+int sceRegMgrGetKeyStr(const char *category, const char *name, char *buf, SceSize size);
 
 /**
  * Set a key's information by category and name
@@ -52,7 +52,7 @@ int sceRegMgrGetKeyStr(const char* category, const char* name, char* buf, const 
  *
  * @return 0 on success, < 0 on error
  */
-int sceRegMgrSetKeyBin(const char *category, const char *name, void *buf, int size);
+int sceRegMgrSetKeyBin(const char *category, const char *name, void *buf, SceSize size);
 
 /**
  * Set a key's information by category and name
@@ -63,7 +63,7 @@ int sceRegMgrSetKeyBin(const char *category, const char *name, void *buf, int si
  *
  * @return 0 on success, < 0 on error
  */
-int sceRegMgrSetKeyInt(const char* category, const char* name, int buf);
+int sceRegMgrSetKeyInt(const char *category, const char *name, int buf);
 
 /**
  * Set a key's information by category and name
@@ -75,7 +75,7 @@ int sceRegMgrSetKeyInt(const char* category, const char* name, int buf);
  *
  * @return 0 on success, < 0 on error
  */
-int sceRegMgrSetKeyStr(const char* category, const char* name, char* buf, const int size);
+int sceRegMgrSetKeyStr(const char *category, const char *name, char *buf, SceSize size);
 
 #ifdef __cplusplus
 }

include/kernel/sblauthmgr.h
@@ -8,7 +8,7 @@ extern "C" {
 #endif
 
 int sceSblAuthMgrClearDmac5Key(int slot_id, int val);
-int sceSblAuthMgrSetDmac5Key(const void *key, size_t keylen, int slot_id, int key_id);
+int sceSblAuthMgrSetDmac5Key(const void *key, SceSize keylen, int slot_id, int key_id);
 
 #ifdef __cplusplus
 }

include/kernel/syscon.h
@@ -106,8 +106,8 @@ int sceSysconCmdSync(SceSysconPacket *packet, int noWait);
  */
 int sceSysconSetPowerMode(int type, int mode);
 
-int sceSysconReadCommand(unsigned short cmd, void *buffer, unsigned int size);
-int sceSysconSendCommand(unsigned short cmd, const void *buffer, unsigned int size);
+int sceSysconReadCommand(unsigned short cmd, void *buffer, SceSize size);
+int sceSysconSendCommand(unsigned short cmd, const void *buffer, SceSize size);
 
 /**
  * Set the debug handlers.
@@ -163,7 +163,7 @@ int sceSysconIsDownLoaderMode(void);
 
 int sceSysconLogStart(void);
 int sceSysconLogStartWaiting(void);
-int sceSysconLogReadData(unsigned short, void *buff, unsigned int size);
+int sceSysconLogReadData(unsigned short, void *buff, SceSize size);
 
 int sceSysconShowModeClear(void);
 int sceSysconShowModeSet(void);
@@ -196,8 +196,8 @@ int sceSysconClearTemperatureLog(int);
 int sceSysconBeginConfigstorageTransaction(void);
 int sceSysconCommitConfigstorageTransaction(void);
 int sceSysconEndConfigstorageTransaction(void);
-int sceSysconLoadConfigstorageScript(unsigned short, const void *buff, unsigned int size);
-int sceSysconVerifyConfigstorageScript(unsigned short, const void *buff, unsigned int size);
+int sceSysconLoadConfigstorageScript(unsigned short, const void *buff, SceSize size);
+int sceSysconVerifyConfigstorageScript(unsigned short, const void *buff, SceSize size);
 
 /**
  * Set the alarm callback, that will be ran when the alarm timer is passed.

Rinnegatamante/vita-headers

include/psp2kern/idstorage.h
@@ -18,7 +18,7 @@ extern "C" {
  *
  * @return 0 on success, < 0 on error.
  */
-int ksceIdStorageReadLeaf(int leafnum, void *buf);
+int ksceIdStorageReadLeaf(SceSize leafnum, void *buf);
   
 /**
  * @param[in] leafnum - 0x0 ~ 0x80 / leafnum > 0x80 = error
@@ -26,7 +26,7 @@ int ksceIdStorageReadLeaf(int leafnum, void *buf);
  *
  * @return 0 on success, < 0 on error.
  */
-int ksceIdStorageWriteLeaf(int leafnum, const void *buf);
+int ksceIdStorageWriteLeaf(SceSize leafnum, const void *buf);
 
 #ifdef __cplusplus
 }

include/psp2kern/kernel/processmgr.h
@@ -39,7 +39,7 @@ int ksceKernelGetProcessLocalStorageAddrForPid(SceUID pid, int key, void **out_a
  * @param[in]   unk Unknown.
  * @return      PID of the launched app on success, < 0 on error.
  */
-SceUID ksceKernelLaunchApp(char* titleid, uint32_t flags, char *path, uint32_t unk);
+SceUID ksceKernelLaunchApp(const char *titleid, uint32_t flags, const char *path, uint32_t unk);
 
 /**
  * @brief       Resume a suspended process.

include/psp2kern/kernel/utils.h
@@ -64,24 +64,24 @@ typedef struct {
 	uint8_t data[976];
 } SceAesContext;
 
-int ksceSha1BlockInit(SceSha1Context *pContext);
-int ksceSha1BlockUpdate(SceSha1Context *pContext, const void *plain, uint32_t len);
-int ksceSha1BlockResult(SceSha1Context *pContext, char *digest);
-int ksceSha1Digest(const void *plain, uint32_t len, char *result);
+int ksceSha1BlockInit(SceSha1Context *ctx);
+int ksceSha1BlockUpdate(SceSha1Context *ctx, const void *plain, SceSize len);
+int ksceSha1BlockResult(SceSha1Context *ctx, void *result);
+int ksceSha1Digest(const void *plain, SceSize len, void *digest);
 
-int ksceSha224BlockInit(SceSha224Context *pContext);
-int ksceSha224BlockUpdate(SceSha224Context *pContext, const void *plain, uint32_t len);
-int ksceSha224BlockResult(SceSha224Context *pContext, char *digest);
-int ksceSha224Digest(const void *plain, uint32_t len, char *result);
+int ksceSha224BlockInit(SceSha224Context *ctx);
+int ksceSha224BlockUpdate(SceSha224Context *ctx, const void *plain, SceSize len);
+int ksceSha224BlockResult(SceSha224Context *ctx, void *result);
+int ksceSha224Digest(const void *plain, SceSize len, void *digest);
 
-int ksceSha256BlockInit(SceSha256Context *pContext);
-int ksceSha256BlockUpdate(SceSha256Context *pContext, const void *plain, uint32_t len);
-int ksceSha256BlockResult(SceSha256Context *pContext, char *digest);
-int ksceSha256Digest(const void *plain, uint32_t len, char *result);
+int ksceSha256BlockInit(SceSha256Context *ctx);
+int ksceSha256BlockUpdate(SceSha256Context *ctx, const void *plain, SceSize len);
+int ksceSha256BlockResult(SceSha256Context *ctx, void *result);
+int ksceSha256Digest(const void *plain, SceSize len, void *digest);
 
-int ksceHmacSha1Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
-int ksceHmacSha224Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
-int ksceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void *plain, uint32_t len, char *result);
+int ksceHmacSha1Digest(const void *key, SceSize key_len, const void *plain, SceSize len, void *digest);
+int ksceHmacSha224Digest(const void *key, SceSize key_len, const void *plain, SceSize len, void *digest);
+int ksceHmacSha256Digest(const void *key, SceSize key_len, const void *plain, SceSize len, void *digest);
 
 /**
  * @param[out] dst - dst buf
@@ -91,7 +91,7 @@ int ksceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void 
  *
  * @return decompressed size on success, < 0 on error.
  */
-int ksceGzipDecompress(void *dst, uint32_t dst_size, const void *src, uint32_t *crc32);
+int ksceGzipDecompress(void *dst, SceSize dst_size, const void *src, uint32_t *crc32);
 
 /**
  * @brief Check magic of Gzip header
@@ -173,7 +173,7 @@ const void *ksceZlibGetCompressedData(const void *src);
  *
  * @return decompressed size on success, < 0 on error.
  */
-int ksceZlibDecompress(void *dst, uint32_t dst_size, const void *src, uint32_t *adler32);
+int ksceZlibDecompress(void *dst, SceSize dst_size, const void *src, uint32_t *adler32);
 
 /**
  * @param[out] dst - dst buf
@@ -183,8 +183,8 @@ int ksceZlibDecompress(void *dst, uint32_t dst_size, const void *src, uint32_t *
  *
  * @return decompressed size on success, < 0 on error.
  */
-int ksceDeflateDecompress(void *dst, uint32_t dst_size, const void *src, const void **next);
-int ksceDeflateDecompressPartial(void *dst, unsigned int dst_size, const void *src, const void **next, SceDeflatePartialInputParam *cbInfo);
+int ksceDeflateDecompress(void *dst, SceSize dst_size, const void *src, const void **next);
+int ksceDeflateDecompressPartial(void *dst, SceSize dst_size, const void *src, const void **next, SceDeflatePartialInputParam *cbInfo);
 
 /**
  * @param[out] ctx - out key data, etc...
@@ -194,9 +194,9 @@ int ksceDeflateDecompressPartial(void *dst, unsigned int dst_size, const void *s
  *
  * @return 0 on success, < 0 on error.
  */
-int ksceAesInit1(SceAesContext *ctx, int blocksize, int keysize, const void *key);
-int ksceAesInit2(SceAesContext *ctx, int blocksize, int keysize, const void *key);
-int ksceAesInit3(SceAesContext *ctx, int blocksize, int keysize, const void *key);
+int ksceAesInit1(SceAesContext *ctx, SceSize blocksize, SceSize keysize, const void *key);
+int ksceAesInit2(SceAesContext *ctx, SceSize blocksize, SceSize keysize, const void *key);
+int ksceAesInit3(SceAesContext *ctx, SceSize blocksize, SceSize keysize, const void *key);
 
 int ksceAesDecrypt1(SceAesContext *ctx, const void *src, void *dst);
 int ksceAesDecrypt2(SceAesContext *ctx, const void *src, void *dst);

include/psp2kern/registrymgr.h
@@ -22,7 +22,7 @@ extern "C" {
  *
  * @return 0 on success, < 0 on error
  */
-int ksceRegMgrGetKeyBin(const char *category, const char *name, void *buf, int size);
+int ksceRegMgrGetKeyBin(const char *category, const char *name, void *buf, SceSize size);
 
 /**
  * Get a key's information by category and name
@@ -33,7 +33,7 @@ int ksceRegMgrGetKeyBin(const char *category, const char *name, void *buf, int s
  *
  * @return 0 on success, < 0 on error
  */
-int ksceRegMgrGetKeyInt(const char* category, const char* name, int* buf);
+int ksceRegMgrGetKeyInt(const char *category, const char *name, int *buf);
 
 /**
  * Get a key's information by category and name
@@ -45,7 +45,7 @@ int ksceRegMgrGetKeyInt(const char* category, const char* name, int* buf);
  *
  * @return 0 on success, < 0 on error
  */
-int ksceRegMgrGetKeyStr(const char* category, const char* name, char* buf, const int size);
+int ksceRegMgrGetKeyStr(const char *category, const char *name, char *buf, SceSize size);
 
 /**
  * Set a key's information by category and name
@@ -57,7 +57,7 @@ int ksceRegMgrGetKeyStr(const char* category, const char* name, char* buf, const
  *
  * @return 0 on success, < 0 on error
  */
-int ksceRegMgrSetKeyBin(const char *category, const char *name, void *buf, int size);
+int ksceRegMgrSetKeyBin(const char *category, const char *name, void *buf, SceSize size);
 
 /**
  * Set a key's information by category and name
@@ -68,7 +68,7 @@ int ksceRegMgrSetKeyBin(const char *category, const char *name, void *buf, int s
  *
  * @return 0 on success, < 0 on error
  */
-int ksceRegMgrSetKeyInt(const char* category, const char* name, int buf);
+int ksceRegMgrSetKeyInt(const char *category, const char *name, int buf);
 
 /**
  * Set a key's information by category and name
@@ -80,7 +80,7 @@ int ksceRegMgrSetKeyInt(const char* category, const char* name, int buf);
  *
  * @return 0 on success, < 0 on error
  */
-int ksceRegMgrSetKeyStr(const char* category, const char* name, char* buf, const int size);
+int ksceRegMgrSetKeyStr(const char *category, const char *name, char *buf, SceSize size);
 
 #ifdef __cplusplus
 }

include/psp2kern/sblauthmgr.h
@@ -14,7 +14,7 @@ extern "C" {
 #endif
 
 int ksceSblAuthMgrClearDmac5Key(int slot_id, int val);
-int ksceSblAuthMgrSetDmac5Key(const void *key, size_t keylen, int slot_id, int key_id);
+int ksceSblAuthMgrSetDmac5Key(const void *key, SceSize keylen, int slot_id, int key_id);
 
 #ifdef __cplusplus
 }

include/psp2kern/syscon.h
@@ -111,8 +111,8 @@ int ksceSysconCmdSync(SceSysconPacket *packet, int noWait);
  */
 int ksceSysconSetPowerMode(int type, int mode);
 
-int ksceSysconReadCommand(unsigned short cmd, void *buffer, unsigned int size);
-int ksceSysconSendCommand(unsigned short cmd, const void *buffer, unsigned int size);
+int ksceSysconReadCommand(unsigned short cmd, void *buffer, SceSize size);
+int ksceSysconSendCommand(unsigned short cmd, const void *buffer, SceSize size);
 
 /**
  * Set the debug handlers.
@@ -168,7 +168,7 @@ int ksceSysconIsDownLoaderMode(void);
 
 int ksceSysconLogStart(void);
 int ksceSysconLogStartWaiting(void);
-int ksceSysconLogReadData(unsigned short, void *buff, unsigned int size);
+int ksceSysconLogReadData(unsigned short, void *buff, SceSize size);
 
 int ksceSysconShowModeClear(void);
 int ksceSysconShowModeSet(void);
@@ -201,8 +201,8 @@ int ksceSysconClearTemperatureLog(int);
 int ksceSysconBeginConfigstorageTransaction(void);
 int ksceSysconCommitConfigstorageTransaction(void);
 int ksceSysconEndConfigstorageTransaction(void);
-int ksceSysconLoadConfigstorageScript(unsigned short, const void *buff, unsigned int size);
-int ksceSysconVerifyConfigstorageScript(unsigned short, const void *buff, unsigned int size);
+int ksceSysconLoadConfigstorageScript(unsigned short, const void *buff, SceSize size);
+int ksceSysconVerifyConfigstorageScript(unsigned short, const void *buff, SceSize size);
 
 /**
  * Set the alarm callback, that will be ran when the alarm timer is passed.