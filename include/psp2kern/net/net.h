SonicMastr/libraries

Add ksceNetConnect

include/kernel/net/net.h
@@ -539,6 +539,7 @@ typedef struct SceNetIcmpHeader {
 int sceNetSocket(const char *name, int domain, int type, int protocol);
 int sceNetAccept(int s, SceNetSockaddr *addr, unsigned int *addrlen);
 int sceNetBind(int s, const SceNetSockaddr *addr, unsigned int addrlen);
+int sceNetConnect(int s, const SceNetSockaddr *name, unsigned int namelen);
 int sceNetListen(int s, int backlog);
 int sceNetRecvfrom(int s, void *buf, unsigned int len, int flags, SceNetSockaddr *from, unsigned int *fromlen);
 int sceNetSendto(int s, const void *msg, unsigned int len, int flags, const SceNetSockaddr *to, unsigned int tolen);

Rinnegatamante/vita-headers

include/psp2kern/net/net.h
@@ -545,6 +545,7 @@ typedef struct SceNetIcmpHeader {
 int ksceNetSocket(const char *name, int domain, int type, int protocol);
 int ksceNetAccept(int s, SceNetSockaddr *addr, unsigned int *addrlen);
 int ksceNetBind(int s, const SceNetSockaddr *addr, unsigned int addrlen);
+int ksceNetConnect(int s, const SceNetSockaddr *name, unsigned int namelen);
 int ksceNetListen(int s, int backlog);
 int ksceNetRecvfrom(int s, void *buf, unsigned int len, int flags, SceNetSockaddr *from, unsigned int *fromlen);
 int ksceNetSendto(int s, const void *msg, unsigned int len, int flags, const SceNetSockaddr *to, unsigned int tolen);