SonicMastr/libraries

Add ksceKernelLaunchApp in processmgr.h (#375)

include/kernel/kernel/processmgr.h
@@ -26,6 +26,16 @@ void *sceKernelGetProcessLocalStorageAddr(int key);
 int sceKernelGetProcessLocalStorageAddrForPid(SceUID pid, int key, void **out_addr, int create_if_doesnt_exist);
 
 /**
+ * @brief       Launch an application
+ * @param[in]   titleid The TitleId of the app to open.
+ * @param[in]   flags Some unknown flags.
+ * @param[in]   path Path of the eboot.bin to launch.
+ * @param[in]   unk Unknown.
+ * @return      PID of the launched app on success, < 0 on error.
+ */
+SceUID sceKernelLaunchApp(char* titleid, uint32_t flags, char *path, uint32_t unk);
+
+/**
  * @brief       Resume a suspended process.
  * @param[in]   pid The process to resume.
  * @return      Zero on success, < 0 on error.

Rinnegatamante/vita-headers

include/psp2kern/kernel/processmgr.h
@@ -32,6 +32,16 @@ void *ksceKernelGetProcessLocalStorageAddr(int key);
 int ksceKernelGetProcessLocalStorageAddrForPid(SceUID pid, int key, void **out_addr, int create_if_doesnt_exist);
 
 /**
+ * @brief       Launch an application
+ * @param[in]   titleid The TitleId of the app to open.
+ * @param[in]   flags Some unknown flags.
+ * @param[in]   path Path of the eboot.bin to launch.
+ * @param[in]   unk Unknown.
+ * @return      PID of the launched app on success, < 0 on error.
+ */
+SceUID ksceKernelLaunchApp(char* titleid, uint32_t flags, char *path, uint32_t unk);
+
+/**
  * @brief       Resume a suspended process.
  * @param[in]   pid The process to resume.
  * @return      Zero on success, < 0 on error.