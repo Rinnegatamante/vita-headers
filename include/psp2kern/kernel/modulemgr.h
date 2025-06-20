SonicMastr/libraries

fixed ksceKernelSearchModuleByName

include/kernel/kernel/modulemgr.h
@@ -152,7 +152,7 @@ int sceKernelStopUnloadModuleForPid(SceUID pid, SceUID modid, SceSize args, void
 int sceKernelMountBootfs(const char *bootImagePath);
 int sceKernelUmountBootfs(void);
 
-int sceKernelSearchModuleByName(const char *module_name, const char *path, int pid);
+int sceKernelSearchModuleByName(const char *module_name);
 
 /**
  * @brief Get the main module for a given process.

Rinnegatamante/vita-headers

include/psp2kern/kernel/modulemgr.h
@@ -158,7 +158,7 @@ int ksceKernelStopUnloadModuleForPid(SceUID pid, SceUID modid, SceSize args, voi
 int ksceKernelMountBootfs(const char *bootImagePath);
 int ksceKernelUmountBootfs(void);
 
-int ksceKernelSearchModuleByName(const char *module_name, const char *path, int pid);
+int ksceKernelSearchModuleByName(const char *module_name);
 
 /**
  * @brief Get the main module for a given process.